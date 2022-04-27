a7b3eaa5a3090a5af10cfc17a7997eaf0f7dad22
Rodrigo <54447826+RodrigoWebDev-BR@users.noreply.github.com>
2022-04-26 22:26:48 -0300

    cadastro_usuario (usuario)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,7 +10,12 @@ const routes: Routes = [
   {
     path: 'folder/:id',
     loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)
+  },  
+  {
+    path: 'usuario',
+    loadChildren: () => import('./usuario/usuario.module').then( m => m.UsuarioPageModule)
   }
+
 ];
 
 @NgModule({
