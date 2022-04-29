59aa3ce8945eb15793b1d9cae180c83246aa3606
THAUA MARCELO SOUZA MELLO <thaua.msmello@senacsp.edu.br>
2022-04-28 22:35:02 -0300

    ahhhh

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,7 +10,11 @@ const routes: Routes = [
   {
     path: 'folder/:id',
     loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)
+  },  {
+    path: 'formacao-educacional',
+    loadChildren: () => import('./formacao-educacional/formacao-educacional.module').then( m => m.FormacaoEducacionalPageModule)
   }
+
 ];
 
 @NgModule({
---
+++ src/app/formacao-educacional/formacao-educacional.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-formacao-educacional',
+  templateUrl: './formacao-educacional.page.html',
+  styleUrls: ['./formacao-educacional.page.scss'],
+})
+export class FormacaoEducacionalPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
