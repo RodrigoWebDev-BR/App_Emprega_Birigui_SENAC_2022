7b2368e5c4090ae278418cff6068a500f7662ac1
SalesPatrick <salespatrick77@gmail.com>
2022-05-03 20:30:03 -0300

    contato ngmodel

--- src/app/contato/contato.page.ts
+++ src/app/contato/contato.page.ts
@@ -1,13 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-contato',
   templateUrl: './contato.page.html',
   styleUrls: ['./contato.page.scss'],
 })
-export class ContatoPage implements OnInit {
+export class ContatoPage implements OnInit {;
 
-  constructor() { }
+  contato = {id: '', contato:''}
+
+  constructor(public rota : Router) {
+
+  }
+
+  addContato(){
+      console.log(this.contato)
+      this.rota.navigate(['folder']);
+  }
 
   ngOnInit() {
   }
