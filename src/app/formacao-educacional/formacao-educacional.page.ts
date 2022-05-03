02a890f1a097adc9e19fcef4ec6a91fc524b1241
THAUA MARCELO SOUZA MELLO <thaua.msmello@senacsp.edu.br>
2022-05-03 19:05:26 -0300

    ta indo

--- src/app/formacao-educacional/formacao-educacional.page.ts
+++ src/app/formacao-educacional/formacao-educacional.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
+import { alertController } from '@ionic/core';
 
 @Component({
   selector: 'app-formacao-educacional',
@@ -7,12 +9,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormacaoEducacionalPage implements OnInit {
 
-  constructor() { }
+  formacao = {};
 
-  ngOnInit() {
+  constructor(public mensagem: AlertController) {
   }
-  adicionarFormacao(){
-    console.log(this.adicionarFormacao);
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
   }
 
+  async adicionarFormacao() {
+
+    const alerta = await this.mensagem.create(
+      {
+        header: "ATENÇÃO",
+        subHeader: "",
+        message: "Informações armazenadas com sucesso",
+        buttons: ["OK"],
+        cssClass: "cssAlerta"
+      }
+    );
+    await alerta.present();
+    console.log(this.formacao);
+  }
+  proximo() {
+    console.log(this.formacao)
+  }
 }
