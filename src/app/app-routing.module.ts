fad635f45fc7d3b0b45ab72dda57ada10ffb5b20
THAUA MARCELO SOUZA MELLO <thaua.msmello@senacsp.edu.br>
2022-05-05 20:02:57 -0300

    td

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,7 +10,8 @@ const routes: Routes = [
   {
     path: 'folder/:id',
     loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)
-  },  {
+  },
+  {
     path: 'formacao-educacional',
     loadChildren: () => import('./formacao-educacional/formacao-educacional.module').then( m => m.FormacaoEducacionalPageModule)
   },
