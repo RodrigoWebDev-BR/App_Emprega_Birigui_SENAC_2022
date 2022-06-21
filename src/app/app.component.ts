86a29fd05628b290afc8279e4cf23aaf66ec6002
SalesPatrick <salespatrick77@gmail.com>
2022-06-20 22:19:16 -0300

    .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   public appPages = [
-    { title: 'Perfil', url: '/folder/usuario', icon: 'person' }
+    { title: 'Perfil', url: '/home', icon: 'person' },
+    { title: 'Notificações', url: '/folder/notificacao', icon: 'notifications' },
+    { title: 'Currículo', url: '/folder/exp-profissional', icon: 'document' },
+    { title: 'Contato', url: '/folder/contato', icon: 'call' },
+    { title: 'Sair', url: '/login', icon: 'power' }
   ];
   public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
   constructor() {}
